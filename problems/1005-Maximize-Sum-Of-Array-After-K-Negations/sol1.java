be8ef131db4434100422b4f6800ce2e945c8ee5e
manav02vor <manavmandal999@gmail.com>
2026-08-12 12:38:16 +0530

    Add sol1: 1005. Maximize Sum Of Array After K Negations (Java)

---
+++ problems/1005-Maximize-Sum-Of-Array-After-K-Negations/sol1.java
@@ -0,0 +1,27 @@
+// ==========================================================
+// 1005. Maximize Sum Of Array After K Negations
+// Difficulty : Easy
+// Language   : Java
+// Solution   : #1
+// Runtime    : 6 ms (Beats 82%)
+// Memory     : 44.7 MB (Beats 28%)
+// Link       : https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/
+// ==========================================================
+
+            }
+
+                }
+                    minIndex = i;
+                if (nums[i] < nums[minIndex]) {
+            for (int i = 1; i < nums.length; i++) {
+
+            int minIndex = 0;
+
+        if (k % 2 == 1) {
+      
+
+        }
+            }
+                }
+                    break;
+            nums[minIndex] = -nums[minIndex];
