b1f7d54cc2b991d63630b49fb74b9de6e500caf7
manav02vor <manavmandal999@gmail.com>
2026-08-19 11:23:53 +0530

    Add sol1: 485. Max Consecutive Ones (Java)

---
+++ problems/0485-Max-Consecutive-Ones/sol1.java
@@ -0,0 +1,29 @@
+// ==========================================================
+// 485. Max Consecutive Ones
+// Difficulty : Easy
+// Language   : Java
+// Solution   : #1
+// Runtime    : 3 ms (Beats 59%)
+// Memory     : 52.8 MB (Beats 11%)
+// Link       : https://leetcode.com/problems/max-consecutive-ones/
+// ==========================================================
+
+        for(int i = 0;i < nums.length;i++){
+            if(nums[i] == 1){
+                count++;
+            }
+                if(nums[i] ==0){
+        int max =0;
+        int count = 0;
+    public int findMaxConsecutiveOnes(int[] nums) {
+class Solution {
+                    count =0;
+                    max = max;
+                }
+                if(count > max){
+                    max = count;
+                }
+                
+            }
+        
+        return max;
